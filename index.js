20cf069840b29887ac124481f5dc7f4192269ea3
luigi01013 <luigi.sa@icloud.com>
2023-10-27 16:10:05 +0200

    new commit learnn

---
+++ index.js
@@ -0,0 +1,23 @@
+// javascript 
+
+
+// stringa
+"ciao"
+
+// stringa
+'ciao'
+
+// stringa
+`ciao`
+
+// oggetto
+var myObject = {
+    name: "Luigi",
+    age: 44,
+} 
+
+// array
+[10, 20, 3.5]
+
+
+
